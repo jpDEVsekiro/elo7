1c34c19eb9ae95bf5e44f0202d656f66adfb77fe
João Pedro de Souza <71463029+jpDEVsekiro@users.noreply.github.com>
2020-11-08 23:38:49 -0300

    terminei a barra de pesquisa, e deixei a menssagem quadro decorativos escrito já quando abrir o app, deu muito trabalho mais está funcionando bem

--- app/src/main/java/com/example/elo7/MainActivity.java
+++ app/src/main/java/com/example/elo7/MainActivity.java
@@ -1,18 +1,28 @@
 package com.example.elo7;
-
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.widget.Toolbar;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.SearchView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
+    private Toolbar toolbar;
+    SearchView mysearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getSupportActionBar().setTitle("");//tiro titulo da action bar
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true); //coloco a seta de voltar na bar
-
-
+        toolbar = (Toolbar)findViewById(R.id.mytoolbar);
+        mysearchView = (SearchView)findViewById(R.id.bar_search);
+        mysearchView.setQuery("quadros decorativos",false);
+        setSupportActionBar(toolbar);
+        Drawable upArrow = getResources().getDrawable(R.drawable.abc_ic_ab_back_material);
+        upArrow.setColorFilter(Color.parseColor("#FFFFFF"), PorterDuff.Mode.SRC_ATOP);
+        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
     }
 }
